fd5704a96538decdd0a9a4407d83171611e6a0ce
RobertBowie <robertb1903@gmail.com>
2015-08-17 13:43:47 -0700

    Updates gameClass.js
    Adds basic gameplay steps to gameClass, allowing for an empty board to
    be generated as well as board printing and piece placing based on column
    selection.

--- gameClass.js
+++ gameClass.js
@@ -1,7 +1,28 @@
 var Game = function(){
-
+  this.board = this.newGame();
 };
 
 Game.prototype.newGame = function() {
-  return //empty board here
+  return [[BLANK, BLANK, BLANK, BLANK, BLANK, BLANK, BLANK],
+          [BLANK, BLANK, BLANK, BLANK, BLANK, BLANK, BLANK],
+          [BLANK, BLANK, BLANK, BLANK, BLANK, BLANK, BLANK],
+          [BLANK, BLANK, BLANK, BLANK, BLANK, BLANK, BLANK],
+          [BLANK, BLANK, BLANK, BLANK, BLANK, BLANK, BLANK],
+          [BLANK, BLANK, BLANK, BLANK, BLANK, BLANK, BLANK]]
+};
+
+Game.prototype.printBoard = function() {
+  console.log(this.board[0] + "\n" + this.board[1] + "\n" + 
+              this.board[2] + "\n" + this.board[3] + "\n" + 
+              this.board[4] + "\n" + this.board[5]);
+};
+
+Game.prototype.placePiece = function(col, piece) {
+//for col === 1 => search board[5][0], board[4][0]...
+  var colTrans = col - 1;
+  for(var i = 5; i >= 0; i--){ //search col for 1st empty
+    if(this.board[i][colTrans] === BLANK){
+      return this.board[i][colTrans] = piece;
+    }
+  }
 };
